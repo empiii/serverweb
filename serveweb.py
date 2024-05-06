29c46202f1cfaac1d8d9894761e62ffb2576ec42
rafaemp <47393806+empiii@users.noreply.github.com>
2024-05-06 18:34:24 -0300

    Add files via upload

---
+++ serveweb.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+
+# Classe para lidar com as requisições
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Response to GET request')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Response to POST request')
+
+def run(server_class=HTTPServer, handler_class=RequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
+
